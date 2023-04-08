775625ecff0f1d0844c7da239df544517347639c
FeederYap <feederyap@gmail.com>
2023-04-08 18:32:06 +0100

    css and jsx

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import './fooder.css'
+import {FaFacebookF} from 'react-icons/fa'
+import {FiInstagram} from 'react-icons/fi'
+import {IoLogoTwitter} from 'react-icons/io'
+
+const Footer = () => {
+  return (
+    <footer>
+      <a href="#" className='footer__logo'>YAP YING JIE</a>
+
+      <ul className='permalinks'>
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#porfolio">Porfolio</a></li>
+        <li><a href="#testimonials">Testimonials</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+
+      <div className='footer__socials'>
+        <a href="https://facebook.com"><FaFacebookF/></a>
+        <a href="https://instagram.com"><FiInstagram/></a>
+        <a href="https://twitter.com"><IoLogoTwitter/></a>
+      </div>
+
+      <div className='footer__copyright'>
+        <small>&copy; YAP YING JIE. All right reserved.</small>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
